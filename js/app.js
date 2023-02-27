d0083048fb31ab2baf197fa69bd3e05bd08bd133
Faizan <53142548+faizan517@users.noreply.github.com>
2023-02-28 01:54:13 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,6 +4,6 @@ setTimeout(() => {
         location.replace("./screens/Home/index.html")
     }
     else{
-        location.replace("./screens/Login/index.html")
+        location.replace("./screens/login/index.html")
     }
 }, 3000);//3 seconds
